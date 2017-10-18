52ec888d125bd931aed8dcb8c7046fc423da5f04
Kevin Robinson <kevinr.zan@gmail.com>
2017-10-17 17:47:18 -0700

    fibonacci implementation

--- src/series.py
+++ src/series.py
@@ -1,4 +1,13 @@
 def fibonacci(n):
     '''return the nth number in the fibonacci series, starting from 1:1.'''
-    total = 0
+    if n <= 0: return 0
+    if n == 1: return 1
+
+    total = 1
+    prev = 0
+    for i in range (n - 1):
+        temp = total
+        total = total + prev
+        prev = temp
+
     return total
