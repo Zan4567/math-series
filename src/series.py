aacb87c5113aa4353344a989ab2baf939ffc27c4
Kevin Robinson <kevinr.zan@gmail.com>
2017-10-17 19:21:35 -0700

    sum_series fn

--- src/series.py
+++ src/series.py
@@ -11,3 +11,31 @@ def fibonacci(n):
         prev = temp
 
     return total
+
+def lucas(n):
+    '''return the nth number in the lucas series, starting from 1:1.'''
+    if n <= 0: return 0
+    if n == 1: return 2
+
+    total = 1
+    prev = 2
+    for i in range (n - 2):
+        temp = total
+        total = total + prev
+        prev = temp
+
+    return total
+
+def sum_series(n, first = 1, second = 1):
+    '''return the nth number in the lucas series, starting from 1:1.'''
+    if n <= 0: return 0
+    if n == 1: return first
+
+    total = second
+    prev = first
+    for i in range (n - 2):
+        temp = total
+        total = total + prev
+        prev = temp
+
+    return total
--- src/test_series.py
+++ src/test_series.py
@@ -5,8 +5,27 @@ FIB_ANSWERS = [
     (9, 34), (10, 55), (11, 89), (12, 144)
 ]
 
+LUC_ANSWERS = [
+    (0, 0), (1, 2), (2, 1), (3, 3), (4, 4), (5, 7), (6, 11), (7, 18), (8, 29),
+    (9, 47), (10, 76), (11, 123), (12, 199)
+]
 
 @pytest.mark.parametrize('n, ans', FIB_ANSWERS)
 def test_fib(n, ans):
     from series import fibonacci
     assert(fibonacci(n) == ans)
+
+@pytest.mark.parametrize('n, ans', LUC_ANSWERS)
+def test_luc(n, ans):
+    from series import lucas
+    assert(lucas(n) == ans)
+
+@pytest.mark.parametrize('n, ans', FIB_ANSWERS)
+def test_sum_series_fib(n, ans):
+    from series import sum_series
+    assert(sum_series(n) == ans)
+
+@pytest.mark.parametrize('n, ans', LUC_ANSWERS)
+def test_sum_series_luc(n, ans):
+    from series import sum_series
+    assert(sum_series(n, 2, 1) == ans)
